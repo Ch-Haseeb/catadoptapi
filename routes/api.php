b78209292aa7cf2fa3e509398a4a416394dea001
Muhammad Haseeb <haseebishtiaq300@gmail.com>
2022-08-19 12:25:31 +0500

    Intial Commit

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CatApi;
+use App\Http\Controllers\CatMedicalApi;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::prefix('auth')->controller(AuthController::class)->group(function () {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+Route::get('show', [CatApi::class, 'index']);
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('store', [CatApi::class, 'store']);
+
+    Route::post('adopt/{id}', [CatApi::class, 'adopt']);
+    Route::post('storemedical',[CatMedicalApi::class,'store']);
+    Route::get('getmedical/{id}', [CatMedicalApi::class, 'getdata']);
+});
+
